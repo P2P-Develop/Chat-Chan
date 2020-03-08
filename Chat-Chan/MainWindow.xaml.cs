1fe348854cf07f94358a58a7143600705e426355
P2PDevelop <p2p-develop@usbc.be>
2020-03-08 22:41:56 +0900

    デザインの枠組みを実装

--- Chat-Chan/MainWindow.xaml.cs
+++ Chat-Chan/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Chat_Chan
 {
@@ -20,9 +7,40 @@ namespace Chat_Chan
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// <see cref="MainWindow"/>のコンストラクタ
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSessionStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEndSession_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 }
