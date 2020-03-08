03699f883501301c7e688aae38aaa8cc097df10e
P2PDevelop <p2p-develop@usbc.be>
2020-03-09 02:51:00 +0900

    enum "Ports"を追加

--- Chat-Chan/MainWindow.xaml.cs
+++ Chat-Chan/MainWindow.xaml.cs
@@ -2,10 +2,23 @@
 
 namespace Chat_Chan
 {
+    /// <summary>
+    /// メッセージをチャットとして送信するかコンソールとして送信するか
+    /// </summary>
     public enum SendMode : byte
     {
         Message = 0,
         Chat = 1
+    }
+
+    /// <summary>
+    /// チャット・コマンド・状態確認の送受信に使うポート
+    /// </summary>
+    public enum Ports : int
+    {
+        Message = 37564,
+        Command = 46573,
+        Status = 41410
     }
 
     /// <summary>
@@ -14,6 +27,7 @@ namespace Chat_Chan
     public partial class MainWindow : Window
     {
         public string message;
+        public string chatmessage;
 
         #region Constructor
 
@@ -81,8 +95,8 @@ namespace Chat_Chan
                     return "400 - 権限がありません";
                 case 401:
                     return "401 - 接続ユーザー数が上限に達しています";
-                case 402:
-                    return "402 - サーバーが起動していません";
+                case 500:
+                    return "500 - サーバーが起動していません";
                 case 418:
                     return "418 - I'm a P2PDevelop";
                 default:
